1a0984c2f03d8496935dafd11911a094c881c116
Tommi Tuura <tommi.tuura@gmail.com>
2013-01-21 02:21:28 +0200

    added testing code for initialdelay

--- js/engine.game.js
+++ js/engine.game.js
@@ -1,18 +1,18 @@
 adeca.engine ? adeca.engine = adeca.engine : adeca.engine = {};
 adeca.engine.game = (function() {
-    var gameFrame = null;
     var runner1 = null;
+    var runner2 = null;
     var tick = function(frame) {
-        gameFrame++;
         
         adeca.canvas.fillStyle="rgb(255,255,255)";
         adeca.canvas.fillRect(0,0,800,800);
 
         runner1.tick();
         runner1.draw(adeca.canvas);
+        runner2.tick();
+        runner2.draw(adeca.canvas);
     };
     var init = function() {
-        gameFrame = 0;
         adeca.canvas.fillStyle="rgb(255,255,255)";
         adeca.canvas.fillRect(0,0,800,800);
 
@@ -22,11 +22,24 @@ adeca.engine.game = (function() {
             animationDelay: 200,
             initialX: 100,
             initialY: 100,
-            finalX: 300,
-            finalY: 300,
+            finalX: 100,
+            finalY: 600,
             travelTime:2000,
-            travelLooping: true,
+            travelLooping: false,
             onTravelLoopEnd: function(self) {console.log('Travel Loop Finished!',new Date(), self)}
+        });
+        
+        runner2 = new adeca.Sprite({
+            imageId: 'runner',
+            animationFrameCount: 5,
+            animationDelay: 70,
+            initialX: 100,
+            initialY: 600,
+            finalX: 600,
+            finalY: 600,
+            travelTime: 1000,
+            travelLooping: false,
+            initialDelay: 2000
         });
     };
     return {
